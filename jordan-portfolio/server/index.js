163ddb620a16609a08d5ef40b53d099cac99e0b0
wiebejordan <wiebe.jordan@yahoo.com>
2020-09-14 13:01:38 -0600

    server updates

--- jordan-portfolio/server/index.js
+++ jordan-portfolio/server/index.js
@@ -4,4 +4,10 @@ const app = express()
 
 app.use(express.json())
 
+app.use(express.static(`${__dirname}/../build`))
+
+app.get('*', (req, res)=>{
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+});
+
 app.listen(5000, () => console.log('Server running on 5000'))
