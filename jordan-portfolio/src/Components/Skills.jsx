f7e3d39bd227a87fa8ee8541b48ce90bfb7d26d3
wiebejordan <wiebe.jordan@yahoo.com>
2020-10-26 12:35:30 -0600

    skills

--- jordan-portfolio/src/Components/Skills.jsx
+++ jordan-portfolio/src/Components/Skills.jsx
@@ -12,21 +12,13 @@ const Skills = () => {
     <List.Item>HTML</List.Item>
     <List.Item>CSS</List.Item>
     <List.Item>Redux</List.Item>
-    <List.Item>Axios</List.Item>
     <List.Item>RESTful API</List.Item>
     <List.Item>Node</List.Item>
-    <List.Item>Express</List.Item>
-    <List.Item>Heroku</List.Item>
     <List.Item>PostgreSQL</List.Item>
     <List.Item>After Effects</List.Item>
     <List.Item>Final Cut Pro X</List.Item>
     <List.Item>Git & Github</List.Item>
-    <List.Item>Bcrypt.js</List.Item>
-    <List.Item>Socket.io</List.Item>
-    <List.Item>NodeMailer</List.Item>
     <List.Item>Photoshop</List.Item>
-    <List.Item>Google Firebase</List.Item>
-    <List.Item>Semantic UI</List.Item>
     <List.Item>Mac OS</List.Item>
     </List>
 
